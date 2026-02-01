e58448ff1a8ea3672f45bf694e8ee0b1a2306a1f
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-02-01 18:53:27 +0600

    first commit

---
+++ mult.js
@@ -0,0 +1,9 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+function mult(a, b, c, d) {
+    const total = a * b * c * d;
+    console.log(total);
+}
+
+mult(6, 4, 7, 90);
